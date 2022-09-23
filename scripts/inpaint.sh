27ebf3ee80e070375a19ff3cb135cac126c0aede
zhihao lin <cl121@illinois.edu>
2022-09-22 22:40:07 -0500

    [0922] fix rand_seed and perform mask&prompt-guided flooding simulation

---
+++ scripts/inpaint.sh
@@ -0,0 +1,5 @@
+python inpaint.py -prompt "The floor is flooding" -seed 1 -samples_num 1 \
+-dir_src  ../weather_nvs/output/0916_playground/frames/normal \
+-dir_mask ../weather_nvs/output/0916_playground/frames/climategan/mask \
+-dir_out  ../weather_nvs/output/0916_playground/frames/diffusion_1
+ 
