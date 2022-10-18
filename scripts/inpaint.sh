504fdc245aa7fdf63d0b9e0c806ac859f01e4028
zhihao lin <cl121@illinois.edu>
2022-10-18 17:11:32 -0500

    [1018] add scripts for inpaint panorama

--- scripts/inpaint.sh
+++ scripts/inpaint.sh
@@ -1,5 +1,4 @@
-python inpaint.py -prompt "The floor is flooding" -seed 1 -samples_num 1 \
+python inpaint.py -prompt "flooding" -seed 1 -samples_num 1 \
 -dir_src  ../weather_nvs/output/0916_playground/frames/normal \
 -dir_mask ../weather_nvs/output/0916_playground/frames/climategan/mask \
--dir_out  ../weather_nvs/output/0916_playground/frames/diffusion_1
- 
+-dir_out  ../weather_nvs/output/0916_playground/frames/diffusion/01
---
+++ scripts/inpaint_pano.sh
@@ -0,0 +1,4 @@
+ python inpaint.py -prompt "a panorama with bright sun in clear sky" -seed 1 -samples_num 1 \
+-dir_src  ../weather_nvs/results/tnt/playground/panorama/rgb \
+-dir_mask ../weather_nvs/results/tnt/playground/panorama/mask \
+-dir_out  ../weather_nvs/results/tnt/playground/panorama/inpaint 
